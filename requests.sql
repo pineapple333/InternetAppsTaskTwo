d963b0ce22b0cea9859fe943ecf062fa1cda7ab1
ev <36419747+pineapple333@users.noreply.github.com>
2021-07-04 19:52:43 -0400

    Changed formatting in 'requests.sql' file

--- requests.sql
+++ requests.sql
@@ -3,5 +3,16 @@ select user.name, role.name from user_role inner join user  on user.id = user_ro
 
 -- function for adding tasks and relationships from BA
 delimiter //
-create procedure insert_task (in in_contents varchar(255), in in_date_from datetime, in in_date_to datetime, in in_parent_id int, in in_project_id int) begin insert into task (contents, date_from, date_to) values (in_contents, in_date_from, in_date_to); SET @last_id = LAST_INSERT_ID(); insert into task_relationship (child_task, parent_task) values (@last_id, in_parent_id); insert into project_task (project_id, task_id) values (in_project_id, @last_id); end;//
+create procedure insert_task 
+    (in in_contents varchar(255), in in_date_from datetime, 
+    in in_date_to datetime, in in_parent_id int, in in_project_id int) 
+    begin 
+        insert into task (contents, date_from, date_to) 
+            values (in_contents, in_date_from, in_date_to); 
+        SET @last_id = LAST_INSERT_ID(); 
+        insert into task_relationship (child_task, parent_task) 
+            values (@last_id, in_parent_id); 
+        insert into project_task (project_id, task_id) 
+            values (in_project_id, @last_id); 
+    end;//
 delimiter ;
